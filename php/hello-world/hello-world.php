db7e11fbf049cedf8bf39d12c2fb8118178d5277
Gabriel Vargas <assuncao.gabriel12@gmail.com>
2017-07-20 23:49:02 +0000

    PHP - Hello world!

---
+++ php/hello-world/hello-world.php
@@ -0,0 +1,16 @@
+<?php
+
+//
+// This is only a SKELETON file for the "Hello World" exercise.
+// It's been provided as a convenience to get you started writing code faster.
+//
+
+function helloWorld($name = null)
+{
+
+	if($name) {
+		return sprintf('Hello, %s!', $name);
+	}
+
+    return 'Hello, World!';
+}
