39cd215f7570ab9802d3b840a268d6c9aef29284
Gabriel Vargas <assuncao.gabriel12@gmail.com>
2020-03-19 22:20:22 -0300

    Dart - reverse string - community solutions

--- dart/reverse-string/lib/reverse_string.dart
+++ dart/reverse-string/lib/reverse_string.dart
@@ -1,3 +1,10 @@
 String reverse(String word) {
   return word.split('').reversed.join();
+
+  // community solutions
+  // https://exercism.io/tracks/dart/exercises/reverse-string/solutions/8debf36edd60434c86216f4180628b99
+
+  // return String.fromCharCodes(word.codeUnits.reversed);
+  // OR
+  // return String.fromCharCodes(word.runes.toList().reversed);
 }
