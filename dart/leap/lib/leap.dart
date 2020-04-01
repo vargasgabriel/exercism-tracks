8bbc341d3d081ed22f632a16acb30299550caa3d
Gabriel Vargas <assuncao.gabriel12@gmail.com>
2020-04-01 20:40:22 -0300

    Dart - Lead - Use arrow syntax on function

--- dart/leap/lib/leap.dart
+++ dart/leap/lib/leap.dart
@@ -1,5 +1,3 @@
 class Leap {
-  bool leapYear(int year) {
-    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
-  }
+  bool leapYear(int year) => year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 }
